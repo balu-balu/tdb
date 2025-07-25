af47d9a71f1f9d782415a24e19f3febfe273619f
balu <balu@noreply.github.com>
2025-07-26 02:00:56 +0800

    Services

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import Home from '@/component/Home/Home'
 import Marquee from '@/component/Home/Marquee'
+import Services from '@/component/Home/Services'
 import React from 'react'
 
 const page = () => {
     return (
         <div>
             <Home/>
-            <Marquee/>
+            <div className='mt-5'>
+                <Marquee/>
+            </div>
+            <div className='mt-10'>
+                <Services/>
+            </div>
+            
         </div>
     )
 }
---
+++ component/Home/Services.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Services = () => {
+	return (
+		<div className='pt-16 pb-16'>
+		<div className='text-center font-medium text-custom-blue text-5xl'>
+			Онцлох үйлчилгээ 
+		</div>
+		<div className='w-[80%] mx-auto'>
+		<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 items-center mt-16'>
+			{/*Card1*/}
+			<div className='bg-white border border-custom-color rounded-4xl shadow-lg py-8 px-6 text-center flex flex-col items-center justify-center'>
+				<div className='bg-card-bg h-[90px] w-[90px] rounded-4xl'></div>
+				<div className='font-medium text-2xl text-custom-blue mt-3 px-6 text-center items-center'>Үнэт цаасны зуучлалын үйлчилгээ</div>
+				<div className='font-light text-sm text-custom-blue-gray mt-3'>Монголын хөрөнгийн биржийн арилжааны системтэй шууд холбогдсон электрон арилжааны системийг ашиглан үнэт цаасны арилжаанд хамгийн түргэн шуурхай, цаг хугацааны хоцрогдолгүйгээр оролцох, үнэт цаасны дансаа удирдах боломжтой.</div>
+			</div>
+			{/*Card2*/}
+			<div className='bg-white border border-custom-color rounded-4xl shadow-lg py-8 px-6 text-center flex flex-col items-center justify-center'>
+				<div className='bg-card-bg h-[90px] w-[90px] rounded-4xl'></div>
+				<div className='font-medium text-2xl text-custom-blue mt-3 px-6 text-center items-center'>Үнэт цаасны зуучлалын үйлчилгээ</div>
+				<div className='font-light text-sm text-custom-blue-gray mt-3'>Монголын хөрөнгийн биржийн арилжааны системтэй шууд холбогдсон электрон арилжааны системийг ашиглан үнэт цаасны арилжаанд хамгийн түргэн шуурхай, цаг хугацааны хоцрогдолгүйгээр оролцох, үнэт цаасны дансаа удирдах боломжтой.</div>
+			</div>
+			{/*Card3*/}
+			<div className='bg-white border border-custom-color rounded-4xl shadow-lg py-8 px-6 text-center flex flex-col items-center justify-center'>
+				<div className='bg-card-bg h-[90px] w-[90px] rounded-4xl'></div>
+				<div className='font-medium text-2xl text-custom-blue mt-3 px-6 text-center items-center'>Үнэт цаасны зуучлалын үйлчилгээ</div>
+				<div className='font-light text-sm text-custom-blue-gray mt-3'>Монголын хөрөнгийн биржийн арилжааны системтэй шууд холбогдсон электрон арилжааны системийг ашиглан үнэт цаасны арилжаанд хамгийн түргэн шуурхай, цаг хугацааны хоцрогдолгүйгээр оролцох, үнэт цаасны дансаа удирдах боломжтой.</div>
+			</div>
+		</div>
+		</div>
+		</div>
+	)
+}
+
+export default Services
