2d90abd44b8085108bcddde2f5b919bfdabb4aae
balu <balu@noreply.github.com>
2025-07-27 20:24:04 +0800

    fix responsive installApp

--- component/Home/InstallApp.tsx
+++ component/Home/InstallApp.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const InstallApp = () => {
-	return (
-		<section className='my-20 relative w-full'>
-			<img src="images/tdb_app.jpg" alt='TDB_app' className='w-full h-[80%] object-cover z-0 layout="fill"'></img>
-			<div className='absolute inset-0 z-10 flex flex-col justify-end items-start p-8 md:p-16'>
-				<button className="bg-white  text-[rgba(41,134,254,1)] font-bold text-lg py-1 px-8 rounded-full shadow-lg hover:bg-gray-100 transition-colors duration-300 ">
-				АПП татах
-				</button>
-			</div>
-		</section>
-	)
-}
+    return (
+        <section className='my-8 relative w-full overflow-hidden'>
+            <Image 
+                src="/images/tdb_app.jpg" 
+                alt='TDB_app' 
+                layout="responsive"
+                width={4}
+                height={3}
+                objectFit="cover"
+                objectPosition="top"
+                className='z-0'
+            />
 
-export default InstallApp
+            <div className='absolute inset-0 z-10 flex flex-col justify-end items-start p-8 md:p-16'>
+                <button className="bg-white text-[rgba(41,134,254,1)] font-bold text-xs md:text-lg py-0.5 px-2 md:py-2 md:px-8 rounded-full shadow-lg hover:bg-gray-100 transition-colors duration-300 border border-[rgba(41,134,254,1)]">
+                    АПП татах
+                </button>
+            </div>
+        </section>
+    );
+};
+
+export default InstallApp;
